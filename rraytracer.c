3031bfffd59bc9e3b18c7a0c29921e403b55c4a4
Ryan Cohen <rcohenprogramming@gmail.com>
2021-10-30 02:29:03 -0400

    Added progress indicator.

--- rraytracer.c
+++ rraytracer.c
@@ -1,26 +1,50 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "rtga/rtga.h"
+
+#define HEIGHT 400
+#define WIDTH 600
 
 int main(void) {
     TgaImage image;
     int success;
 
+    // Set image struct to all 0s
     memset(&image, 0, sizeof(image));
 
-    success = tga_alloc(UNCOMPRESSED_TRUE_COLOR_IMAGE, 600, 400, 24, &image);
+    // Allocate image
+    success = tga_alloc(UNCOMPRESSED_TRUE_COLOR_IMAGE, WIDTH, HEIGHT, 24, &image);
     if (success != TGA_SUCCESS) {
         printf("Error %i occured while allocating image.\n", success);
         return 1;
     }
 
+    // Trace rays for each pixel in image
+    for (int y = 0; y < HEIGHT; ++y) {
+        printf("\r\e[2K");
+        printf("\rScanlines remaining: %i", HEIGHT - y);
+        fflush(stdout);
+        for (int x = 0; x < WIDTH; ++x) {
+            TgaColor color = COLOR24(255, 0, 128);
+            tga_set_pixel(&image, x, y, color);
+        }
+        usleep(5000);
+    }
+    printf("\r\e[2K");
+    printf("\rRaytracer finished!\n");
+
+
+    // Write image to file
     success = tga_write_file(&image, "raytracer.tga");
     if (success != TGA_SUCCESS) {
         printf("Error occured while writing image to a file.\n");
         return 2;
     }
 
+    // Free image
     tga_free(&image);
+
     return 0;
 }
